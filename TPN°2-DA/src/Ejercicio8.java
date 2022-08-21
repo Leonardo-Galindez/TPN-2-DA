02b13165ab9ec314173a715986b32a14e3e09eb3
@HerWar <88455461+LeonardoGalindez@users.noreply.github.com>
2022-08-21 02:03:44 -0300

    cambios

--- TPN°2-DA/src/Ejercicio8.java
+++ TPN°2-DA/src/Ejercicio8.java
@@ -1,18 +1,19 @@
 import java.util.Scanner;
+
 public class Ejercicio8 {
-    public static void main(String[]args){
-        Scanner sc= new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         int cantPos;
         String palabraMayor;
-        palabraMayor="";
-        cantPos=ModulosArreglo.DefCantPosArray();
-        String CadenaArray[]=new String[cantPos]; 
-        CadenaArray=ModulosArreglo.CargaArregloString(cantPos);
-        for(int i=0;i<CadenaArray.length;i++){
-            if(palabraMayor.length() < CadenaArray[i].length()){
-                palabraMayor=CadenaArray[i];
+        palabraMayor = "";
+        cantPos = ModulosArreglo.DefCantPosArray();
+        String CadenaArray[] = new String[cantPos];
+        CadenaArray = ModulosArreglo.CargaArregloString(cantPos);
+        for (int i = 0; i < CadenaArray.length; i++) {
+            if (palabraMayor.length() < CadenaArray[i].length()) {
+                palabraMayor = CadenaArray[i];
             }
         }
-        System.out.println("La palabra mas larga del arreglo es:"+palabraMayor);
+        System.out.println("La palabra mas larga del arreglo es:" + palabraMayor);
     }
 }
