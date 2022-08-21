31ccc5eb8e11e4980f17351595aca4ebe144ea49
@HerWar <88455461+LeonardoGalindez@users.noreply.github.com>
2022-08-21 01:56:16 -0300

    ejercicio 13 terminado

--- TPN°2-DA/src/Ejercicio13.java
+++ TPN°2-DA/src/Ejercicio13.java
@@ -2,6 +2,27 @@ import java.util.Scanner;
 public class Ejercicio13 {
     public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
-        
+
+        int cant,j;
+        j=0;
+
+        cant=ModulosArreglo.DefCantPosArray();
+
+        char Carac[]=new char[cant];
+
+        Carac=ModulosArreglo.CargaArreglochar(cant);
+
+        char VocalesArray[]=new char[cant];
+
+        for(int i=0;i<VocalesArray.length;i++){
+            if(Carac[i]=='a'||Carac[i]=='e'||Carac[i]=='i'||Carac[i]=='o'||Carac[i]=='u'||Carac[i]=='A'||Carac[i]=='E'||Carac[i]=='I'||Carac[i]=='O'||Carac[i]=='U'){
+                VocalesArray[i]= Carac[i];
+            }
+            j++;
+        }
+        for(int i=0;i<VocalesArray.length;i++){
+            System.out.print(VocalesArray[i]);
+        }
+
     }
 }
